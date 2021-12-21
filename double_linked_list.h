37fee6e24cc010845cdfba1db093a670201a4553
ammicco1 <84914236+ammicco@users.noreply.github.com>
2021-12-21 09:09:05 +0100

    init

---
+++ double_linked_list.h
@@ -0,0 +1,34 @@
+#include "element.h"
+
+typedef struct linked_list{
+    element *elem;
+    struct linked_list *next, *prev;
+} linked_list;
+
+linked_list *inzialize_linked_list(element *elem);
+
+void add_node(linked_list **list, element *elem);
+
+int delete_node(linked_list **list, key *chiave);
+
+int delete_node_from_index(linked_list **list, int index);
+
+void print_all_list(linked_list *list);
+
+bool list_include(linked_list *list, key *key);
+
+void swap_node(linked_list **list, int index1, int index2);
+
+int node_count(linked_list *list);
+
+linked_list *get_node_by_index(linked_list *list, int index);
+
+int get_max_key(linked_list *list);
+
+int get_min_key(linked_list *list);
+
+linked_list *get_prev_node(linked_list *list);
+
+int partition(linked_list **list, int first_ind, int last_ind);
+
+void quicksort(linked_list **list, int first_ind, int last_ind);
---
+++ makefile
@@ -0,0 +1,21 @@
+FLAGS = -Wall -ansi -pedantic
+OBJS = double_list_main.o key.o element.o double_linked_list.o
+EXEC = double_list_main
+
+$(EXEC): $(OBJS)
+	gcc $(FLAGS) -o $(EXEC) $(OBJS)
+
+$(EXEC).o: $(EXEC).c
+	gcc $(FLAGS) -c $(EXEC).c
+
+double_linked_list.o: double_linked_list.c
+	gcc $(FLAGS) -c double_linked_list.c
+
+element.o: element.c
+	gcc $(FLAGS) -c element.c
+
+key.o: key.c
+	gcc $(FLAGS) -c key.c
+
+clean: 
+	rm -f *.o $(EXEC) $(OBJS)
